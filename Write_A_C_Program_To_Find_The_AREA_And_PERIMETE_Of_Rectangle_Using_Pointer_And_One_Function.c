4424d9e40f81e77082b4dd87d47cd60dcd4b3b93
Keya-Das <keya5392d@gmail.com>
2025-02-07 02:29:58 +0530

    Add files via upload

---
+++ Write_A_C_Program_To_Find_The_AREA_And_PERIMETE_Of_Rectangle_Using_Pointer_And_One_Function.c
@@ -0,0 +1,18 @@
+// question no-34
+#include <stdio.h>
+void calculateRectangle(float *length, float *width, float *area, float *perimeter)
+{
+    *area = (*length) * (*width);
+    *perimeter = 2 * ((*length) + (*width));
+}
+int main() {
+    float length, width, area, perimeter;
+    printf("Enter the length of the rectangle: ");
+    scanf("%f", &length);
+    printf("Enter the width of the rectangle: ");
+    scanf("%f", &width);
+    calculateRectangle(&length, &width, &area, &perimeter);
+    printf("\nArea of the rectangle: %.2f\n", area);
+    printf("Perimeter of the rectangle: %.2f\n", perimeter);
+    return 0;
+}
