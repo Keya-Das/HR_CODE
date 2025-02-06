aefb3ca9ed7bfe95b2a138b76bccb1a7b82908bd
Keya-Das <keya5392d@gmail.com>
2025-02-07 02:19:09 +0530

    Add files via upload

---
+++ Increasing_And_Decreasing_Half_Pyramid.c
@@ -0,0 +1,18 @@
+// question no-32
+#include <stdio.h>
+int main() {
+    int n = 4;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
