7338a2bdeaf67d06831acab9f8f42e836383e062
Keya-Das <keya5392d@gmail.com>
2025-02-07 02:22:40 +0530

    Add files via upload

---
+++ Polynomial_Series_with_Alternating_Signs.c
@@ -0,0 +1,18 @@
+// question no-18
+#include <stdio.h>
+#include <math.h> 
+int main() {
+    int n;
+    float X, sum = 0;
+    printf("Enter value of X: ");
+    scanf("%f", &X);
+    printf("Enter the number of terms n: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        
+        sum += (pow(-1, i+1)) * i * pow(X, i);
+    }
+    printf("Sum of the series: %.6f\n", sum);
+
+    return 0;
+}
